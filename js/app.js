00ff85faa49b5a3416f11a1d05a4d9db824b84a4
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-02-13 15:47:47 +0100

    added the play function for switching one screen to another screen!

---
+++ js/app.js
@@ -0,0 +1,19 @@
+
+// between moving secreen option 1 
+// function play(){
+//    //hide the home screen 
+//    const homeSection = document.getElementById('home-screen');
+//    homeSection.classList.add('hidden');
+
+//    //show the home screen 
+//    const playGroundSection = document.getElementById('play-ground');
+//    playGroundSection.classList.remove('hidden')
+// }
+
+// between moving secreen option 2 
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    
+}
