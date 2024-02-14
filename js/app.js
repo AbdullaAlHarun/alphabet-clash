9e4ceab8ca4484620089fed37e9a9d24b17c8ff5
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-02-14 16:04:48 +0100

    checked mathch or not!

--- js/app.js
+++ js/app.js
@@ -24,6 +24,52 @@ function handleKeyboardKeyUpEvent(event){
     const currentAlphabet = currentAlphabetElement.innerText;
     const expectedAlphabet = currentAlphabet.toLowerCase();
 
-    
+    // check matched or not 
+
+    if(playerPressed === expectedAlphabet){
+
+        //get the current score option : 1 
+        //  const currentScoreElement = document.getElementById('current-score');
+        //  const currentScoreText = currentScoreElement.innerText;
+        //  const currrentScore =  parseInt(currentScoreText);
+        //  console.log(currrentScore);
+        
+        //  // increase the score by 1 
+        // const newScore = currrentScore + 1;
+
+        // // show the update score  
+        // currentScoreElement.innerText = newScore;
+        const currrentScore =getTextElementValueById('current-score');
+        const updatedScore = currrentScore + 1;
+        setTextElementValueById('current-score', updatedScore);
+        //get the current score option : 2
+        
+        // start a new round
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    } else {
+
+        // option 1 : 
+        //  //step -1 :  get the current life number 
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
+
+        // //step-2: reduce the life count 
+        // const newLife = currentLife - 1;
+
+        // //step 3: display the update life count 
+        // currentLifeElement.innerText = newLife;
+
+
+        //option: 2 
+        const currentLife = getTextElementValueById('current-life');
+        const updatedLife = currentLife -1; 
+        setTextElementValueById('current-life', updatedLife);
+
+        if(updatedLife === 0){
+            gameOver();     
+        }
+    }
 
 }
